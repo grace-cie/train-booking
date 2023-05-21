153bafe6a9f0a0c63e20a60f3ef7716a2aeb76ce
grace-cie <rirei1415@gmail.com>
2023-05-21 11:37:20 +0800

    add unit testing

--- app/controllers/BookController.php
+++ app/controllers/BookController.php
@@ -1,91 +1,102 @@
 <?php
-// namespace BookingsInitName; for testing
 
-// require('../model/DBConn.php');
-// include_once('../model/DBConn.php');
+// Comment this section if not teting
+namespace BookingsInitName;
 
-class Bookings extends DbConnection {
-     public function __construct(){
-  
+use mysqli;
+
+// end
+
+class Bookings extends DbConnection
+{
+     public function __construct()
+     {
+
           parent::__construct();
      }
 
-     public function addBooking($user_id ,$from, $to, $dep, $ret, $type){
+     public function addBooking($user_id, $from, $to, $dep, $ret, $type)
+     {
           $sql = "INSERT INTO bookings(`customer_id`, `b_from`, `b_to`, `dep_date`, `ret_date`, `typet`) VALUES ('$user_id','$from','$to','$dep','$ret','$type')";
 
-          if($this->connection->query($sql)){
+          if ($this->connection->query($sql)) {
                return true;
           } else {
                return false;
           }
-     } 
-     
-     public function getBooking($id){
+     }
+
+     public function getBooking($id)
+     {
           $sql = "SELECT * FROM `bookings` WHERE `customer_id` = $id";
           $query = $this->connection->query($sql);
-          if($query->num_rows > 0){
+          if ($query->num_rows > 0) {
                $data = array();
-               while($row = $query->fetch_assoc()){
+               while ($row = $query->fetch_assoc()) {
                     $data[] = $row;
                }
-               return  $data;
+               return $data;
           } else {
                return false;
           }
      }
 
-     public function payBooking($id, $amount, $curr_amount, $price, $bookId){
-          if(!($curr_amount < $price)){
+     public function payBooking($id, $amount, $curr_amount, $price, $bookId)
+     {
+          if (!($curr_amount < $price)) {
                $sql = "UPDATE `bookings` SET `amount_paid` = $amount, `pay_status` = 'paid' WHERE `id` = $bookId";
                $query = $this->connection->query($sql);
-               if($query){
+               if ($query) {
                     $sql2 = "UPDATE `users` SET `acc_balance` = `acc_balance` - $amount WHERE `id` = $id";
-                    $query2 = $this->connection->query($sql2);
-                    return true;
+                    $query2 = $this->connection->query($sql2) ? true : false;
+                    return $query2;
                }
-               // $row = $query->fetch_array();
           } else {
                return false;
           }
      }
 
-     public function getMoneyAmount($id){
+     public function getMoneyAmount($id)
+     {
           $sql = "SELECT * FROM `users` WHERE `id` = $id";
           $query = $this->connection->query($sql);
 
           $row = $query->fetch_array();
 
-          return $row;   
+          return $row;
      }
 
-     public function escape_string($value){
-  
+     public function escape_string($value)
+     {
+
           return $this->connection->real_escape_string($value);
-          
+
      }
 }
 
-class DbConnection{
- 
+class DbConnection
+{
+
      private $host = 'localhost';
      private $username = 'root';
      private $password = 'KmJsYDN)vLSiLzwj';
      private $database = 'flightbook';
-  
+
      protected $connection;
-  
-     public function __construct(){
-  
-         if (!isset($this->connection)) {
-  
-             $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
-  
-             if (!$this->connection) {
-                 echo 'Cannot connect to database server';
-                 exit;
-             }            
-         }    
-  
-         return $this->connection;
+
+     public function __construct()
+     {
+
+          if (!isset($this->connection)) {
+
+               $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+               if (!$this->connection) {
+                    echo 'Cannot connect to database server';
+                    exit;
+               }
+          }
+
+          return $this->connection;
      }
- } 
+}
---
+++ tests/BookingTest.php
@@ -0,0 +1,129 @@
+<?php
+//./vendor/bin/phpunit --testdox 
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . "/../app/sma/Calc.php";
+require __DIR__ . "/../app/Calcu.php";
+require __DIR__ . "/../app/controllers/BookController.php";
+// require __DIR__ . "/../app/model/DBConn.php";
+
+class BookingTest extends TestCase
+{
+    // public function testAdd(){
+    //      $calc = new App\Calc;
+    //      $res = $calc->addd(1,1);
+
+    //      $this->assertEquals(2, $res);
+    // }
+
+    // public function testAddd(){
+    //      $calc = new Appp\Calcu;
+    //      $res = $calc->addd(1,3);
+
+    //      $this->assertEquals(4, $res);
+    // }
+    public function getController()
+    {
+        return new BookingsInitName\Bookings;
+    }
+
+
+    /**
+     * @group addBooking
+     */
+    public function testShouldReturnTheBookingDetails()
+    {
+        $bookingController = $this->getController();
+        $res = $bookingController->addBooking(1, 'test-from', 'test-to', 'test-depdate', 'test-retdate', 'test-type');
+
+        $this->assertEquals(true, $res);
+    }
+
+    public function testShouldReturnBookingsIfIdExist()
+    {
+        $bookingController = $this->getController();
+        $fixture = array(
+            array(
+                'id' => '1',
+                'customer_id' => '1',
+                'b_from' => 'Danao',
+                'b_to' => 'Consolacion',
+                'dep_date' => '05/01/2023',
+                'ret_date' => '05/17/2023',
+                'typet' => 'ow',
+                'pay_status' => 'paid',
+                'amount_paid' => '100',
+            ),
+            array(
+                'id' => '18',
+                'customer_id' => '1',
+                'b_from' => 'Li-loan',
+                'b_to' => 'Mandaue',
+                'dep_date' => '05/01/2023',
+                'ret_date' => '05/17/2023',
+                'typet' => 'ow',
+                'pay_status' => 'paid',
+                'amount_paid' => '100',
+            ),
+        );
+
+        $res = $bookingController->getBooking(1);
+
+        foreach ($fixture as $index => $booking) {
+            $fixtureKeys = array_keys($booking);
+            $resKeys = array_keys($res[$index]);
+
+            $this->assertSame($fixtureKeys, $resKeys);
+        }
+    }
+
+    public function testShouldReturnFalseIfIdDidntExist()
+    {
+        $bookingController = $this->getController();
+        $res = $bookingController->getBooking(1000);
+        $this->assertEquals(false, $res);
+    }
+
+    public function testShouldReturnTrueIfBalanceIsEnoughAndPaymentIsSuccessAndBalanceUpdated()
+    {
+        $bookingController = $this->getController();
+        $res = $bookingController->payBooking(1, 100, 9999, 100, 41);
+        $this->assertEquals(true, $res);
+    }
+
+    public function testShouldReturnFalseIfBalanceIsEnough()
+    {
+        $bookingController = $this->getController();
+        $res = $bookingController->payBooking(1, 100, 0, 100, 41);
+        $this->assertEquals(false, $res);
+    }
+
+    public function testShouldReturnUserDetailsToGetMoneyBalance()
+    {
+        $bookingController = $this->getController();
+        $fixture = array(
+            '0' => '1',
+            'id' => '1',
+            '1' => 'rirei0',
+            'username' => 'rirei0',
+            '2' => '123123',
+            'password' => '123123',
+            '3' => 'Testfname',
+            'fname' => 'Testfname',
+            '4' => 'Testlname',
+            'lname' => 'Testlname',
+            '5' => 'sample@email.com',
+            'email' => 'sample@email.com',
+            '6' => 'test',
+            'usertype' => 'ow',
+            '7' => '9999',
+            'acc_balance' => '9999',
+        );
+
+        $res = $bookingController->getMoneyAmount(1);
+        $resKeys = array_keys($res);
+        $fixtureKeys = array_keys($fixture);
+        $this->assertEquals($resKeys, $fixtureKeys);
+    }
+
+}
